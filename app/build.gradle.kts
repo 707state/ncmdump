4c9a698479e0d9bf58b28f7618a3badf38ccb4e7
Jask <2713427119@qq.com>
2025-10-13 09:35:21 +0800

    UPDATE: 音频播放器和NCM文件转换都可以工作

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.netease.ncmdump"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.netease.ncmdump"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))  // 只编这些架构
+        }
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+    externalNativeBuild {
+        cmake {
+            path=file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.documentfile)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.animation)
+    implementation(libs.androidx.foundation)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/cpp/src/lib/libncmdump.cpp
@@ -0,0 +1,64 @@
+#include "libncmdump.h"
+
+#include <jni.h>
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+extern "C" {
+API NeteaseCrypt *CreateNeteaseCrypt(const char *path) {
+  fs::path fPath = fs::u8path(path);
+  return new NeteaseCrypt(fPath.u8string());
+}
+
+API int Dump(NeteaseCrypt *neteaseCrypt, const char *outputPath) {
+  try {
+    neteaseCrypt->Dump(outputPath);
+  } catch (const std::invalid_argument &e) {
+    return 1;
+  }
+  return 0;
+}
+
+API void FixMetadata(NeteaseCrypt *neteaseCrypt) {
+  neteaseCrypt->FixMetadata();
+}
+
+API void DestroyNeteaseCrypt(NeteaseCrypt *neteaseCrypt) {
+  delete neteaseCrypt;
+}
+}
+extern "C" JNIEXPORT jstring
+Java_com_example_player2_MainActivity_stringFromJNI(JNIEnv *env,
+                                                    jobject /* this */) {
+  std::string hello = "Hello from C++";
+  return env->NewStringUTF(hello.
+
+                           c_str()
+
+  );
+}
+extern "C" JNIEXPORT jint Java_com_netease_ncmdump_NcmBridge_convertAll(
+    JNIEnv *env, jobject thiz, jstring jInputFolder, jstring jOutputFolder) {
+  const char *inputFile = env->GetStringUTFChars(jInputFolder, nullptr);
+  const char *outputFolder = env->GetStringUTFChars(jOutputFolder, nullptr);
+
+  int converted = 0;
+
+  try {
+    // 直接处理传入的单个 NCM 文件
+    auto ncm = CreateNeteaseCrypt(inputFile);
+    Dump(ncm, outputFolder);
+    FixMetadata(ncm);
+    DestroyNeteaseCrypt(ncm);
+    converted = 1; // 成功处理一个文件
+  } catch (const std::exception &e) {
+    std::cerr << "Caught exception: " << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Caught unknown exception!" << std::endl;
+  }
+
+  env->ReleaseStringUTFChars(jInputFolder, inputFile);
+  env->ReleaseStringUTFChars(jOutputFolder, outputFolder);
+}
